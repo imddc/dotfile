a8eb417aa19a2823abfcf4139b810d0f3f7a4313
Du Cheng <easyhoo2426@163.com>
2024-06-25 23:21:43 +0800

    chore: some edit

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -4,13 +4,14 @@ return {
   event = { 'BufReadPre', 'BufNewFile' }, -- to disable, comment this out
   config = function()
     require('conform').setup {
+
       formatters_by_ft = {
         lua = { 'stylua' },
-        vue = { 'prettier' },
-        javascript = { 'prettier' },
-        typescript = { 'prettier' },
-        javascriptreact = { 'prettier' },
-        typescriptreact = { 'prettier' },
+        -- vue = { 'prettier' },
+        -- javascript = { 'prettier' },
+        -- typescript = { 'prettier' },
+        -- javascriptreact = { 'prettier' },
+        -- typescriptreact = { 'prettier' },
         css = { 'prettier' },
         html = { 'prettier' },
         json = { 'prettier' },
--- nvim/lua/plugins/lint.lua
+++ nvim/lua/plugins/lint.lua
@@ -8,19 +8,13 @@ return {
       typescript = { 'eslint_d' },
       javascriptreact = { 'eslint_d' },
       typescriptreact = { 'eslint_d' },
-      vue = { 'eslint' },
+      vue = { 'eslint_d' },
     }
 
-    local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
     vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
-      group = lint_augroup,
       callback = function()
         lint.try_lint()
       end,
     })
-
-    vim.keymap.set('n', '<leader>l', function()
-      lint.try_lint()
-    end, { desc = 'Trigger lingting for current file' })
   end,
 }
